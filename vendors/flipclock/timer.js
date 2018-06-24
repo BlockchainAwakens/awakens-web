cc9a3fa241b66eb106f5c3e35a5528b9cbe09da2
Weiyi Zeng <weiyizeng@weiyizeng-macbookpro.roam.corp.google.com>
2018-06-23 22:54:33 -0700

    change time

--- vendors/flipclock/timer.js
+++ vendors/flipclock/timer.js
@@ -1,7 +1,7 @@
 var today = new Date();
 
 var timer = function() {};
-timer.countdownDate = new Date("Sat Jul 28 2018 08:00:00 GMT-0700");
+timer.countdownDate = new Date("Sat Aug 4 2018 08:00:00 GMT-0700");
 
 /*
 * Get thing started
